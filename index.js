fcc30ac52396313e4aca79a271c0a976e3ba5553
Annie Sahlén <annieimsahlen@gmail.com>
2024-10-23 14:42:28 +0200

    Fix connection

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send({ success: `It's working!` })
 })
 
